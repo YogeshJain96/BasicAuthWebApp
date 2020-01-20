94090dd54907435c4b2e95dab118000a9544b06c
YogeshJain96 <yogeshjainyugi@gmail.com>
2020-01-21 02:14:54 +0530

    Added JWT,cors,other apis

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-
+const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const db =
   "mongodb+srv://userName:Password@basics-qzlwg.mongodb.net/test?retryWrites=true&w=majority";
+
 const User = require("../models/user");
 mongoose.connect(db, err => {
   if (err) {
@@ -12,6 +13,23 @@ mongoose.connect(db, err => {
     console.log("Connected to DB");
   }
 });
+
+function verifyToken(req, res, next) {
+  if (!req.headers.authorization) {
+    return res.status(401).send("Unauthorized request");
+  }
+  let token = req.headers.authorization.split(" ")[1];
+  if (token === "null") {
+    return res.status(401).send("Unauthorized request");
+  }
+  let payload = jwt.verify(token, "secretKey");
+  if (!payload) {
+    return res.status(401).send("Unauthorized request");
+  }
+  req.userId = payload.subject;
+  next();
+}
+
 //Base api/
 router.get("/", (req, res) => {
   res.send("From API Route");
@@ -24,7 +42,9 @@ router.post("/register", (req, res) => {
     if (error) {
       console.log("error in saving" + error);
     } else {
-      res.status(200).send(registeredUser);
+      let payload = { subject: registeredUser._id };
+      let token = jwt.sign(payload, "secretKey");
+      res.status(200).send({ token });
     }
   });
 });
@@ -41,7 +61,10 @@ router.post("/login", (req, res) => {
       } else if (user.password !== userData.password) {
         res.status(401).send("Invalid password");
       } else {
-        res.status(200).send(user);
+        //res.status(200).send(user);
+        let payload = { subject: user._id };
+        let token = jwt.sign(payload, "secretKey");
+        res.status(200).send({ token });
       }
     }
   });
@@ -83,7 +106,7 @@ router.get("/commonlist", (req, res) => {
   res.json(commonList);
 });
 
-router.get("/speciallist", (req, res) => {
+router.get("/speciallist", verifyToken, (req, res) => {
   let specialList = [
     {
       _id: "1",
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-//const cors = require('cors')
+const cors = require("cors");
 //const path = require('path');
 
 const PORT = 3000;
@@ -8,10 +8,10 @@ const PORT = 3000;
 const api = require("./routes/api");
 
 const app = express();
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use("/api", api);
-//app.use(cors());
+
 //app.use(express.static(path.join(__dirname, "dist")));
 
 app.get("/", function(req, res) {
